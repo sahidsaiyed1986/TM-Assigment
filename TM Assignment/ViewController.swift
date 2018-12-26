b76e4f699f6133221252bdfd1996e23ada24aa58
intersilindia <ssaiyed@intersilindia.com>
2018-12-26 12:59:47 +0530

    Initial Commit

---
+++ TM Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TM Assignment
+//
+//  Created by apple on 12/26/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TM AssignmentTests/TM_AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  TM_AssignmentTests.swift
+//  TM AssignmentTests
+//
+//  Created by apple on 12/26/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import XCTest
+@testable import TM_Assignment
+
+class TM_AssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
